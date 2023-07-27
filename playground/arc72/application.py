551dfef6cf34582498f51325043918dc01c46de6
Stéphane <sudoweezy@gmail.com>
2023-07-27 10:13:44 +0200

    pip8

--- playground/arc72/application.py
+++ playground/arc72/application.py
@@ -8,23 +8,27 @@ Bytes256 = pt.abi.StaticBytes[Literal[256]]
 
 INDEX = 0
 MAX_TOKENS = pt.Int(10)
-ZERO_ADDRESS =  pt.Global.zero_address()
+ZERO_ADDRESS = pt.Global.zero_address()
 APPROVAL = False
 URL = "https://github.com/algorandfoundation/ARCs"
 URL = URL + (256-len(URL))*" "
 URL = pt.Bytes(URL)
+
+
 class Token(pt.abi.NamedTuple):
     owner: pt.abi.Field[pt.abi.Address]
     uri: pt.abi.Field[Bytes256]
     controller: pt.abi.Field[pt.abi.Address]
 
+
 class Control(pt.abi.NamedTuple):
     owner: pt.abi.Field[pt.abi.Address]
     controller: pt.abi.Field[pt.abi.Address]
 
+
 class Arc72:
     # tokenList = storage.BoxList(pt.abi.Address, MAX_TOKENS)
-    index =  beaker.GlobalStateValue(
+    index = beaker.GlobalStateValue(
         stack_type=pt.TealType.uint64,
         descr="index of token",
         static=False,
@@ -37,7 +41,9 @@ class Arc72:
     #     descr="An int stored for each account that opts in",
     # )
 
+
 app = beaker.Application("ARC72", state=Arc72())
+
 
 @app.external(read_only=True)
 def arc72_ownerOf(tokenId: pt.abi.Uint64, *, output: pt.abi.Address) -> pt.Expr:
@@ -45,6 +51,8 @@ def arc72_ownerOf(tokenId: pt.abi.Uint64, *, output: pt.abi.Address) -> pt.Expr:
         app.state.token_box[tokenId].store_into(t := Token()),
         t[0].store_into(output)
     )
+
+
 @app.external(read_only=True)
 def arc72_tokenURI(tokenId: pt.abi.Uint64, *, output: Bytes256) -> pt.Expr:
     return pt.Seq(
@@ -52,63 +60,56 @@ def arc72_tokenURI(tokenId: pt.abi.Uint64, *, output: Bytes256) -> pt.Expr:
         t[1].store_into(output)
     )
 
-
-# def _events(methodName: pt.abi.String, arguments: pt.abi.String):
-#     return pt.MethodSignature()
-
-
-
-
 @app.external
 def transferTo(to: pt.abi.Address, tokenId: pt.abi.Uint64):
     return pt.Seq(
         (t := Token()).decode(app.state.token_box[tokenId].get()),
         (o := pt.abi.Address()).set(to),
-        (u := pt.abi.make(Bytes256)).set(t.uri),     
-        (c := pt.abi.Address()).set(t.controller),        
-        t.set(o,u,c),
+        (u := pt.abi.make(Bytes256)).set(t.uri),
+        (c := pt.abi.Address()).set(t.controller),
+        t.set(o, u, c),
         app.state.token_box[tokenId].set(t)
-    )  
+    )
+
 
 @app.external
 def arc72_transferFrom(_from: pt.abi.Address, to: pt.abi.Address, tokenId: pt.abi.Uint64):
     pt.MethodSignature("arc72_Transfer(address,address,uint64)")
     return pt.Seq(
         (t := Token()).decode(app.state.token_box[tokenId].get()),
-        (c := pt.abi.Address()).set(t.controller),   
-        (o := pt.abi.Address()).set(t.owner),    
-        (operator := pt.abi.Address()).set(pt.Txn.sender()),  
+        (c := pt.abi.Address()).set(t.controller),
+        (o := pt.abi.Address()).set(t.owner),
+        (operator := pt.abi.Address()).set(pt.Txn.sender()),
         (key := Control()).set(operator, _from),
-        pt.If( pt.Or(
-                    pt.Eq(pt.Txn.sender(), _from.get()),
-                    pt.Eq(c.get(), pt.Txn.sender()),
-                )).Then( 
-        transferTo(to, tokenId) 
+        pt.If(pt.Or(
+            pt.Eq(pt.Txn.sender(), _from.get()),
+            pt.Eq(c.get(), pt.Txn.sender()),
+        )).Then(
+            transferTo(to, tokenId)
         ).Else(
             pt.Assert(app.state.control_box[key].exists()),
-            transferTo(to, tokenId) 
+            transferTo(to, tokenId)
         ),
-    )  
+    )
 
-
-    
 
 @app.external
-def arc72_approve(approved: pt.abi.Address, tokenId: pt.abi.Uint64): #TODO
+def arc72_approve(approved: pt.abi.Address, tokenId: pt.abi.Uint64):  # TODO
     pt.MethodSignature("arc72_Approval(address,address,uint64)")
 
     return pt.Seq(
-    (t := Token()).decode(app.state.token_box[tokenId].get()),
-    (o := pt.abi.Address()).set(t.owner),
-    (u := pt.abi.make(Bytes256)).set(t.uri),
-    (c := pt.abi.Address()).set(approved),
-    t.set(o,u,c),
-    pt.Assert(pt.Eq(pt.Txn.sender(), o.get())),
+        (t := Token()).decode(app.state.token_box[tokenId].get()),
+        (o := pt.abi.Address()).set(t.owner),
+        (u := pt.abi.make(Bytes256)).set(t.uri),
+        (c := pt.abi.Address()).set(approved),
+        t.set(o, u, c),
+        pt.Assert(pt.Eq(pt.Txn.sender(), o.get())),
         app.state.token_box[tokenId].set(t)
-        )
+    )
+
 
 @app.external
-def arc72_setApprovalForAll(operator: pt.abi.Address, *, output: pt.abi.Bool): #TODO
+def arc72_setApprovalForAll(operator: pt.abi.Address, *, output: pt.abi.Bool):  # TODO
     pt.MethodSignature("arc72_ApprovalForAll(address,address,bool)")
 
     return pt.Seq(
@@ -116,17 +117,17 @@ def arc72_setApprovalForAll(operator: pt.abi.Address, *, output: pt.abi.Bool): #
         (key := Control()).set(operator, f),
         (exist := pt.abi.Bool()).set(app.state.control_box[key].exists()),
         (b := pt.abi.Bool()).set(pt.Int(1)),
-        pt.If(exist.get()).Then(     
-                
-                pt.Pop(app.state.control_box[key].delete()),
-                output.set(pt.Int(0)),
-            
+        pt.If(exist.get()).Then(
+
+            pt.Pop(app.state.control_box[key].delete()),
+            output.set(pt.Int(0)),
+
         ).Else(
-                app.state.control_box[key].set(b),        
-                output.set(pt.Int(1)),
-            )
+            app.state.control_box[key].set(b),
+            output.set(pt.Int(1)),
         )
-    
+    )
+
 
 @app.external
 def mint(to: pt.abi.Address):
@@ -140,15 +141,18 @@ def mint(to: pt.abi.Address):
         app.state.token_box[i].set(t),
         transferTo(o, i),
         app.state.index.set(i.get() + pt.Int(1)),
-    )  
+    )
+
 
 @app.external(read_only=True)
 def arc72_totalSupply(*, output: pt.abi.Uint64):
     return output.set(app.state.index.get())
-    
+
+
 @app.external(read_only=True)
 def arc72_tokenByIndex(index: pt.abi.Uint64, *, output: pt.abi.Uint64):
     return output.set(index.get())
+
 
 def compute_min_balance(members: int):
     """Compute the min balance for the app to hold the boxes we need"""
@@ -161,11 +165,12 @@ def compute_min_balance(members: int):
         * members  # cover min bal for balance boxes we might create
         + (
             consts.BOX_FLAT_MIN_BALANCE
-            + (members * pt.abi.size_of(pt.abi.Address) * consts.BOX_BYTE_MIN_BALANCE)
+            + (members * pt.abi.size_of(pt.abi.Address)
+               * consts.BOX_BYTE_MIN_BALANCE)
         )  # cover min bal for member list box
     )
+
 
 @app.opt_in
 def opt_in() -> pt.Expr:
     return app.initialize_local_state()
-
--- playground/arc72/demo.py
+++ playground/arc72/demo.py
@@ -140,42 +140,5 @@ def main() -> None:
     # arc72_ownerOf = app_client.call(application.arc72_ownerOf, tokenId=0, boxes=[(0, 0)])
     # print(arc72_ownerOf.return_value) 
 
-    # # Call the method to add a new member
-    # boxes = [(0, encoding.decode_address(member.address)), (0, b"members")]
-    # app_client.call(application.add_member, addr=member.address, boxes=boxes)
-
-    # # We can read a box directly
-    # box_contents = app_client.get_box_contents(encoding.decode_address(member.address))
-    # decoded_box_contents = abi.ABIType.from_string("uint64").decode(box_contents)
-    # print(decoded_box_contents)
-
-    # # Or call the read-only method we've implemented
-    # # to get the parsed version
-    # result = app_client.call(application.read_balance, addr=member.address, boxes=boxes)
-    # print(result.return_value)
-
-    # # Set the balance and read it back out
-    # app_client.call(
-    #     application.set_balance, addr=member.address, amount=100, boxes=boxes
-    # )
-    # result = app_client.call(application.read_balance, addr=member.address, boxes=boxes)
-    # print(result.return_value)
-
-    # # fill the box with some data
-    # box_name = "mybox"
-    # app_client.call(
-    #     application.fill_box,
-    #     box_name=box_name,
-    #     box_data=["hello", "world"],
-    #     boxes=[(0, box_name.encode())],
-    # )
-    # read it back out
-    # box_contents = app_client.get_box_contents(box_name.encode())
-    # print(box_contents)
-    # decode it using the sdk ABI methods
-    # decoded_box_contents = abi.ABIType.from_string("string[]").decode(box_contents)
-    # print(decoded_box_contents)
-
-
 if __name__ == "__main__":
     main()
